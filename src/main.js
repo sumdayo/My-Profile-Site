db192de8d47ab4372479699122574cf5216b6821
sumdayo <wkvmgc961uy56@gmail.com>
2025-06-17 17:56:06 +0900

    fix script

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // タブ切り替え
-document.querySelectorAll('.tab-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const targetId = btn.dataset.target;
+//document.querySelectorAll('.tab-btn').forEach(btn => {
+  //btn.addEventListener('click', () => {
+    //const targetId = btn.dataset.target;
 
-    document.querySelectorAll('.tab-content').forEach(content => {
-      content.classList.remove('active');
-    });
-    document.getElementById(targetId).classList.add('active');
-  });
-});
+    //document.querySelectorAll('.tab-content').forEach(content => {
+      //content.classList.remove('active');
+    //});
+    //document.getElementById(targetId).classList.add('active');
+  //});
+//});
